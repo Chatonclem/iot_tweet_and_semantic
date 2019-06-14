e42eb65e417b252ec1cc9fa0911b6e7ed34da939
antoine <antoine.levy@iscool-e.com>
2019-06-14 13:07:48 +0200

    update main with proper code

--- main.py
+++ main.py
@@ -1,68 +1,91 @@
+import sys
 import boto3
 import tweepy
 from gtts import gTTS
 import os
-
-# # !/usr/bin/env python3.6
-# import RPi.GPIO as GPIO
-# import time
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-# GPIO.setup(16, GPIO.OUT)
-# GPIO.setup(20, GPIO.OUT)
-# GPIO.setup(21, GPIO.OUT)
-
+import RPi.GPIO as GPIO
+import time
 try:
     import json
 except ImportError:
     import simplejson as json
 
-# Variables that contains the user credentials to access Twitter API
-TWITTER_ACCESS_TOKEN = 'XXX'
-TWITTER_ACCESS_SECRET = 'XXX'
-TWITTER_CONSUMER_KEY = 'XXX'
-TWITTER_CONSUMER_SECRET = 'XXX'
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(20, GPIO.OUT)
+    GPIO.setup(21, GPIO.OUT)
+    # Variables that contains the user credentials to access Twitter API
+    TWITTER_ACCESS_TOKEN = 'XXX'
+    TWITTER_ACCESS_SECRET = 'XXX'
+    TWITTER_CONSUMER_KEY = 'XXX'
+    TWITTER_CONSUMER_SECRET = 'XXX'
+    # Setup tweepy to authenticate with Twitter credentials:
+    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    # Create the api to connect to twitter with your creadentials
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
-# Setup tweepy to authenticate with Twitter credentials:
-auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
-# Create the api to connect to twitter with your creadentials
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+def quit_properly():
+    GPIO.output(16, GPIO.LOW)
+    GPIO.output(20, GPIO.LOW)
+    GPIO.output(21, GPIO.LOW)
 
-for tweet in tweepy.Cursor(api.search, q='#trump').items(1):
-    text = tweet._json["text"]
-    print(">>> DEBUG text : " + str(text))
-    comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1')
-    lang = json.loads(json.dumps(comprehend.detect_dominant_language(Text=text), sort_keys=True, indent=4))["Languages"][0]["LanguageCode"]
-    print(">>> DEBUG language : " + str(lang))
-    sentiment = json.loads(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode=lang), sort_keys=True, indent=4))["Sentiment"]
-    if sentiment == "NEGATIVE":
-        print(">>> DEBUG Sentiment : " + str(sentiment))
-        # GPIO.output(16, GPIO.HIGH)
-        # time.sleep(5)
-        # GPIO.output(16, GPIO.LOW)
-    elif sentiment == "POSITIVE":
-        print(">>> DEBUG Sentiment : " + str(sentiment))
-        # GPIO.output(20, GPIO.HIGH)
-        # time.sleep(5)
-        # GPIO.output(20, GPIO.LOW)
-    elif sentiment == "NEUTRAL":
-        print(">>> DEBUG Sentiment : " + str(sentiment))
-        # GPIO.output(21, GPIO.HIGH)
-        # time.sleep(5)
-        # GPIO.output(21, GPIO.LOW)
+
+def loop(hashtag="trump", debug=False):
+    for tweet in tweepy.Cursor(api.search, q='#trump').items(10):
+        text = tweet._json["text"]
+        print(">>> DEBUG text : " + str(text))
+        comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1')
+        lang = json.loads(json.dumps(comprehend.detect_dominant_language(Text=text), sort_keys=True, indent=4))["Languages"][0]["LanguageCode"]
+        print(">>> DEBUG language : " + str(lang))
+        sentiment = json.loads(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode=lang), sort_keys=True, indent=4))["Sentiment"]
+        if sentiment == "NEGATIVE":
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(20, GPIO.HIGH)
+        elif sentiment == "POSITIVE":
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(21, GPIO.HIGH)
+        elif sentiment == "NEUTRAL":
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(16, GPIO.HIGH)
+        else:
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(16, GPIO.HIGH)
+            GPIO.output(20, GPIO.HIGH)
+            GPIO.output(21, GPIO.HIGH)
+        
+        tts=gTTS(text=text,lang=lang)
+        tts.save("tmp.mp3")
+        os.system("mpg321 tmp.mp3")
+
+        if sentiment == "NEGATIVE":
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(20, GPIO.LOW)
+        elif sentiment == "POSITIVE":
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(21, GPIO.LOW)
+        elif sentiment == "NEUTRAL":
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(16, GPIO.LOW)
+        else:
+            print(">>> DEBUG Sentiment : " + str(sentiment))
+            GPIO.output(16, GPIO.LOW)
+            GPIO.output(20, GPIO.LOW)
+            GPIO.output(21, GPIO.LOW)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 3:
+        print("Error too much arguments is pass to the function")
+        print("usage : python " + str(sys.argv[0]) + " <hashtag>  <debug>")
+    setup()
+    if len(sys.argv) == 2:
+        loop(hashtag=sys.argv[1], debug=False)
+    if len(sys.argv) == 3:
+        loop(hashtag=sys.argv[1], debug=True)
     else:
-        print(">>> DEBUG Sentiment : " + str(sentiment))
-        # GPIO.output(16, GPIO.HIGH)
-        # time.sleep(5)
-        # GPIO.output(16, GPIO.LOW)
-        # GPIO.output(20, GPIO.HIGH)
-        # time.sleep(5)
-        # GPIO.output(20, GPIO.LOW)
-        # GPIO.output(21, GPIO.HIGH)
-        # time.sleep(5)
-        # GPIO.output(21, GPIO.LOW)
+        loop()
 
-    tts=gTTS(text=text,lang=lang)
-    tts.save("test.mp3")
-    os.system("start test.mp3")
+    quit_properly()
